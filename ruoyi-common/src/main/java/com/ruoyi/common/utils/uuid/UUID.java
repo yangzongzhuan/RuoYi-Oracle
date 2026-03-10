ab0b5df1569dd976a96cdfbbf28ad5c7d69c0057
RuoYi <yzz_ivy@163.com>
2026-03-10 20:25:16 +0800

    项目升级到 Spring Boot 4

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/uuid/UUID.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/uuid/UUID.java
@@ -66,7 +66,7 @@ public final class UUID implements java.io.Serializable, Comparable<UUID>
     }
 
     /**
-     * 获取类型 4（伪随机生成的）UUID 的静态工厂。
+     * 获取类型 4（伪随机生成的）UUID 的静态工厂。 
      * 
      * @return 随机生成的 {@code UUID}
      */
